28dadc8f4bf32fc9737a9b1509574855313181a9
iwatamac <hallofzv164@gmail.com>
2021-05-10 17:33:19 +0900

    ユーザー新規登録の単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
-
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
+    validates :nickname, uniqueness:{ case_sensitive: false}, length: { maximum: 40 }
+    validates :password, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: "Include both letters and numbers"}
     validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full_width characters."}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full_width characters."}
     validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full_width katakana characters."}
     validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full_width katakana characters."}
+    validates :birthday
   end       
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.free_email}
+    password              {'1a' + Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {"あア阿"}
+    first_name            {"あア阿"}
+    last_name_kana        {"ア"}
+    first_name_kana       {"ア"}
+    birthday              {Faker::Date.between_except(from: '2014-09-23', to: '2014-09-25', excepted: '2015-01-24')}
+  end
+end

235d903d3340f5d1be9dbbc0c807f9e94c3e209d
iwatamac <hallofzv164@gmail.com>
2021-05-11 17:37:36 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_area
+  belongs_to :delivery_days
+  belongs_to :responsibility
+
   belongs_to :user
   has_one_attached :image
-  
+
 end

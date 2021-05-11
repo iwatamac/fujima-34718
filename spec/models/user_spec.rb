458ef33bae1e1d4e35598f13a15c08787e39bd30
iwatamac <hallofzv164@gmail.com>
2021-05-11 14:28:18 +0900

    テストコード記述順の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,50 +27,6 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include "Nickname can't be blank"
       end
-      it 'emailが空だと登録できない' do
-        @user.email = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include "Email can't be blank"
-      end
-      it 'passwordが空だと登録できない' do
-        @user.password = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include "Password can't be blank"
-      end
-      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
-        @user.password_confirmation = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
-      end
-      it 'last_nameが空だと登録できない' do
-        @user.last_name = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include "Last name can't be blank",
-                                                      'Last name is invalid. Input full_width characters.'
-      end
-      it 'first_nameが空だと登録できない' do
-        @user.first_name = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include "First name can't be blank",
-                                                      'First name is invalid. Input full_width characters.'
-      end
-      it 'last_name_kanaが空だと登録できない' do
-        @user.last_name_kana = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include "Last name kana can't be blank",
-                                                      'Last name kana is invalid. Input full_width katakana characters.'
-      end
-      it 'first_name_kanaが空だと登録できない' do
-        @user.first_name_kana = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include "First name kana can't be blank",
-                                                      'First name kana is invalid. Input full_width katakana characters.'
-      end
-      it 'birthdayが空だと登録できない' do
-        @user.birthday = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include "Birthday can't be blank"
-      end
       it 'nicknameが40文字以上では登録できない' do
         @user.nickname = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
         @user.valid?
@@ -83,6 +39,11 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors.full_messages).to include 'Nickname has already been taken'
       end
+      it 'emailが空だと登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email can't be blank"
+      end
       it '重複したemailが存在する場合登録できない' do
         @user.save
         another_user = FactoryBot.build(:user)
@@ -94,6 +55,16 @@ RSpec.describe User, type: :model do
         @user.email = 'aaaaaagmail.com'
         @user.valid?
         expect(@user.errors.full_messages).to include 'Email is invalid'
+      end
+      it 'passwordが空だと登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password can't be blank"
+      end
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
       end
       it 'passwordが5文字以下では登録できない' do
         @user.password = '1aaaa'
@@ -119,26 +90,55 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include "Password Include both letters and numbers"
       end
+      it 'last_nameが空だと登録できない' do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Last name can't be blank",
+                                                      'Last name is invalid. Input full_width characters.'
+      end
       it 'last_nameが全角(漢字、カタカナ、ひらがな)でなければ登録できない' do
         @user.last_name = 'aaaaaa'
         @user.valid?
         expect(@user.errors.full_messages).to include 'Last name is invalid. Input full_width characters.'
+      end
+      it 'first_nameが空だと登録できない' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "First name can't be blank",
+                                                      'First name is invalid. Input full_width characters.'
       end
       it 'first_nameが全角(漢字、カタカナ、ひらがな)でなければ登録できない' do
         @user.first_name = 'aaaaaa'
         @user.valid?
         expect(@user.errors.full_messages).to include 'First name is invalid. Input full_width characters.'
       end
+      it 'last_name_kanaが空だと登録できない' do
+        @user.last_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Last name kana can't be blank",
+                                                      'Last name kana is invalid. Input full_width katakana characters.'
+      end
       it 'last_name_kanaが全角(カタカナ)でなければ登録できない' do
         @user.last_name_kana = 'aあ阿'
         @user.valid?
         expect(@user.errors.full_messages).to include 'Last name kana is invalid. Input full_width katakana characters.'
+      end
+      it 'first_name_kanaが空だと登録できない' do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "First name kana can't be blank",
+                                                      'First name kana is invalid. Input full_width katakana characters.'
       end
       it 'first_name_kanaが全角(カタカナ)でなければ登録できない' do
         @user.first_name_kana = 'aあ阿'
         @user.valid?
         expect(@user.errors.full_messages).to include 'First name kana is invalid. Input full_width katakana characters.'
       end
+      it 'birthdayが空だと登録できない' do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Birthday can't be blank"
+      end  
     end
   end
 end

ef26ed895d34d53245cbbf4fbbdad737270eec0b
iwatamac <hallofzv164@gmail.com>
2021-05-13 11:55:18 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,28 @@ class Item < ApplicationRecord
   belongs_to :delivery_area
   belongs_to :delivery_days
   belongs_to :responsibility
-
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :responsibility_id
+    validates :delivery_area_id
+    validates :delivery_days_id
+    validates :price
+    validates :image
+  end
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :condition_id
+    validates :responsibility_id
+    validates :delivery_area_id
+    validates :delivery_days_id
+  end
+
 
 end

d54378b0b46fff515b787139e62aa9b2f1b92eb9
iwatamac <hallofzv164@gmail.com>
2021-05-17 19:16:12 +0900

    @itemに修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,6 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless item.user == current_user
+    redirect_to action: :index unless @item.user == current_user
   end
 end

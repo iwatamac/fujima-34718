45acd87fda440477a99f3ba99b3f30526589e253
iwatamac <hallofzv164@gmail.com>
2021-05-17 19:13:18 +0900

    set_itemメソッド、move_to_index追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :move_to_index]
   before_action :move_to_index, only: [:edit, :update]
 
   def index
@@ -46,7 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
     redirect_to action: :index unless item.user == current_user
   end
 end

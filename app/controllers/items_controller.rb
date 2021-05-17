7842ae20ed1e65527e284c717f14fe4345015f5d
iwatamac <hallofzv164@gmail.com>
2021-05-17 17:21:27 +0900

    他者が編集URLを入力したらトップページに遷移される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -41,5 +41,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :responsibility_id, :delivery_area_id,
                                  :delivery_days_id, :price).merge(user_id: current_user.id)
+  end
+  
+  def move_to_index
+    item = Item.find(params[:id])
+    unless item.user == current_user
+      redirect_to action: :index
+    end
   end
 end

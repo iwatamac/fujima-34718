9dd768e4d9b69cf20de6c2a32930e40a2b5198d2
iwatamac <hallofzv164@gmail.com>
2021-05-20 16:54:46 +0900

    住所保存機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,17 @@
 class OrdersController < ApplicationController
+
   def index
-    @item = Item.new
   end
 
+  def create
+    @order = Order.create
+    Address.create(address_params)
+    redirect_to root_path
+  end
 
+  private
+
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(order_id: @order.id)
+  end
 end

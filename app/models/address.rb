f8f01c06edf0711a27622ee8cf013901161a0cc3
iwatamac <hallofzv164@gmail.com>
2021-05-21 10:45:43 +0900

    バリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :order
 
-  
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :prefecture_id, numericality: {other_than: 0 }
+    validates :city
+    validates :house_number
+    validates :phone_number
+  end
+
 end

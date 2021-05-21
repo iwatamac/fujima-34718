9596a9863176cc7f09ea0cf1ff32d853bb00ef62
iwatamac <hallofzv164@gmail.com>
2021-05-21 11:36:33 +0900

    order_controllerの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,22 @@
 class OrdersController < ApplicationController
 
   def index
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order = Order.create
-    Address.create(address_params)
-    redirect_to root_path
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
-  def address_params
-    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(order_id: @order.id)
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id)
   end
 end

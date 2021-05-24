ba27a24f97ca2f7c44cfc3b5cf6d862283872b4b
iwatamac <hallofzv164@gmail.com>
2021-05-24 16:57:21 +0900

    クレジットカード決済機能

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
@@ -11,6 +11,7 @@ class OrderAddress
                              length: { minimum: 10, maximum: 11, message: 'is too short' }
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save

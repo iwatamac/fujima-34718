667274aa104d8b59d3158f2c1d286858fe21f7ae
iwatamac <hallofzv164@gmail.com>
2021-05-25 10:44:59 +0900

    商品出品者がURLで購入ページに遷移できないようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,9 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def create
